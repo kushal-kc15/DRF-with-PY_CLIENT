f375abb6507b5cb94e70c5f2b75f3c021c01d990
kushal kc <kckushal164@gmail.com>
2026-01-16 21:34:07 +0545

    feat: Add basic Python client script for local API interaction.

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,14 +1,15 @@
 import requests
 
 # endpoint="https://httpbin.org/status/200"
-endpoint="https://httpbin.org/anything"
+# endpoint="https://httpbin.org/anything"
+endpoint="http://localhost:8000/"
 
 get_response=requests.get(endpoint,json={"query":"python"}) #HTTP Request
-# print(get_response.text) #print raw tect response
+print(get_response.text) #print raw tect response
+print(get_response.status_code)
 
 #HTTP Request -> Html
 #REST API HTTP Request -> JSON
 
 #JavaScript Object Notation ~ Python Dict
 # print(get_response.json())
-print(get_response.status_code)
