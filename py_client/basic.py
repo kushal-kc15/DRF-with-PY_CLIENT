2d25693e110fe669877501f91793b0e5dbae6327
kushal kc <kckushal164@gmail.com>
2026-01-18 07:26:50 +0545

    Django Model Instance to Dictionary

--- py_client/basic.py
+++ py_client/basic.py
@@ -6,11 +6,12 @@ endpoint="http://localhost:8000/api"
 
 get_response=requests.get(endpoint,params={"product_id ":123}) #HTTP Request
 # print(get_response.text) #print raw tect response
-print(get_response.json())
+# print(get_response.headers)
+# print(get_response.json())
 # print(get_response.status_code)
 
 #HTTP Request -> Html
 #REST API HTTP Request -> JSON
 
 #JavaScript Object Notation ~ Python Dict
-# print(get_response.json())
+print(get_response.json())
