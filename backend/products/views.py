bfeb8c27a844542e95664bfbf0ffd0c1d0cd6e64
kushal kc <kckushal164@gmail.com>
2026-01-21 19:58:17 +0545

    Add Session Authentication and permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,39 +1,41 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework import mixins
+from rest_framework import mixins,permissions,authentication
 
-  
-class ProductListCreate(generics.ListCreateAPIView):
-  queryset=Product.objects.all()
-  serializer_class=ProductSerializer
+#generics view
+# class ProductListCreate(generics.ListCreateAPIView):
+#   queryset=Product.objects.all()
+#   serializer_class=ProductSerializer
 
-  def perform_create(self,serializer):
-    title=serializer.validated_data.get("title")
-    content=serializer.validated_data.get("content") or None
-    if content is None:
-      content=title
-    serializer.save(content=content)
+#   def perform_create(self,serializer):
+#     title=serializer.validated_data.get("title")
+#     content=serializer.validated_data.get("content") or None
+#     if content is None:
+#       content=title
+#     serializer.save(content=content)
 
-class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-  queryset=Product.objects.all()
-  serializer_class=ProductSerializer
-  lookup_field="pk"
+# class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+#   queryset=Product.objects.all()
+#   serializer_class=ProductSerializer
+#   lookup_field="pk"
 
-  def perform_update(self,serializer):
-    instance=serializer.save()
-    if instance.title and not instance.content:
-      instance.content=instance.title
-    instance.save()
+#   def perform_update(self,serializer):
+#     instance=serializer.save()
+#     if instance.title and not instance.content:
+#       instance.content=instance.title
+#     instance.save()
 
 #not using this method instead we are using ProductListCreateView above
-class ProductListView(generics.ListAPIView):
-  queryset=Product.objects.all()
-  serializer_class=ProductSerializer
+# class ProductListView(generics.ListAPIView):
+#   queryset=Product.objects.all()
+#   serializer_class=ProductSerializer
 
 class ProductListCreateView(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
   queryset=Product.objects.all()
   serializer_class=ProductSerializer
+  authentication_classes=[authentication.SessionAuthentication]
+  permission_classes=[permissions.IsAuthenticatedOrReadOnly]
 
   def get(self,request,*args,**kwargs):
     return self.list(request,*args,**kwargs)  
